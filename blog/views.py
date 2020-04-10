ecc32765a8d2b1659b845d72141107ef24088652
Eszter <eszterviva@gmail.com>
2020-04-10 10:48:04 +0200

    fix indenting error

--- blog/views.py
+++ blog/views.py
@@ -11,7 +11,7 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
- def post_new(request):
+def post_new(request):
     form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
